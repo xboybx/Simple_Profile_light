3da36fcf594144682326f8764dbef7351e2ecede
xboybx <146360441+xboybx@users.noreply.github.com>
2023-10-02 01:46:19 +0530

    Add files via upload

---
+++ profile2.js
@@ -0,0 +1,7 @@
+ const ham_burger=document.querySelector(".js-hamburger");
+ const navbaroptions=document.querySelector(".contact_logos");
+
+
+ ham_burger.addEventListener("click",()=>{
+    navbaroptions.classList.toggle('active')
+ });
